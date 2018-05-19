9b22bd601c93cbda3aba36afce0279d06d20d56d
emiliesedziol <sedziole@gmail.com>
2018-05-19 14:07:25 -0400

    changes to the buttons

--- OddorEven/app/src/main/java/com/example/oddoreven/MainActivity.java
+++ OddorEven/app/src/main/java/com/example/oddoreven/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.oddoreven;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mDisplayNumber;
 
+    private int mResult = 0;
+
+    private TextView mDisplayResult;
+
+    private Button mEvenButton;
+    private Button mOddButton;
+    private Button mAgainButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,37 +34,70 @@ public class MainActivity extends AppCompatActivity {
         mDisplayNumber = (TextView) findViewById(R.id.numberTextView);
 
         // get ref to result text
-        TextView displayResult = (TextView) findViewById(R.id.numberTextView);
+        mDisplayResult = (TextView) findViewById(R.id.resultTextView);
 
         //get references to odd button and setup the click listener
-        Button oddButton = (Button) findViewById(R.id.oddButton);
-        oddButton.setOnClickListener(new View.OnClickListener() {
+        mOddButton = (Button) findViewById(R.id.oddButton);
+        mOddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if (checkResult(false)) {
+                    mDisplayResult.setText("Try again, this isn't an odd number");
+                    mDisplayResult.setTextColor(Color.parseColor("#ff0000"));
+                } else {
+                    mDisplayResult.setText("Yes, it is Odd");
+                    mDisplayResult.setTextColor(Color.BLUE);
+                }
+                changeGuessButtonState(false);
+                changeAgainButtonState();
             }
         });
 
         // get references to even  and setup the click listener
-        Button evenButton = (Button) findViewById(R.id.evenButton);
-        evenButton.setOnClickListener(new View.OnClickListener() {
+        mEvenButton = (Button) findViewById(R.id.evenButton);
+        mEvenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if (checkResult(true)) {
+                    mDisplayResult.setText("Good job, it is Even");
+                    mDisplayResult.setTextColor(Color.BLUE);
+                }
+                else {
+                    mDisplayResult.setText("hmmm, are you sure");
+                    mDisplayResult.setTextColor(Color.parseColor("#ff0000"));
+                }
+                changeGuessButtonState(false);
+                changeAgainButtonState();
             }
         });
 
         // get again button and setup the click listener
-        Button againButton = (Button) findViewById(R.id.againButton);
-        againButton.setOnClickListener(new View.OnClickListener() {
+        mAgainButton = (Button) findViewById(R.id.againButton);
+        mAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 numberGenerator();
+                mDisplayResult.setText("NEW Number, is it even or odd??");
+
+                mDisplayResult.setTextColor(Color.parseColor("#000000"));
+                changeGuessButtonState(true);
+
+                mOddButton.setBackgroundColor(Color.parseColor("#00bfff"));
+                mOddButton.setTextColor(Color.BLACK);
+
+                mEvenButton.setBackgroundColor(Color.parseColor("#00bfff"));
+                mEvenButton.setTextColor(Color.BLACK);
+
+                mAgainButton.setBackgroundColor(Color.BLACK);
+                mAgainButton.setTextColor(Color.WHITE);
             }
         });
 
         // generate first number
         numberGenerator();
+        mDisplayNumber.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 100);
     }
 
     // method to generate a random number
@@ -62,12 +105,47 @@ public class MainActivity extends AppCompatActivity {
         Random r = new Random();
         int low = 1;
         int high = 99;
-        int result = r.nextInt(high) + low;
+        mResult = r.nextInt(high) + low;
 
-        Log.d(TAG, "new number: " + result);
+        Log.d(TAG, "new number: " + mResult);
 
-        String displayNumberAsString = Integer.toString(result);
+        String displayNumberAsString = Integer.toString(mResult);
         mDisplayNumber.setText(displayNumberAsString);
 
+    }
+
+    boolean checkResult(boolean userGuess) {
+        boolean userGuessCorrectly = false;
+        boolean numberEvenOrOdd = false;
+        if ((mResult % 2) == 0) {
+            numberEvenOrOdd = true;
+        }
+        if ((userGuess && numberEvenOrOdd) || (!userGuess && numberEvenOrOdd) ) {
+            userGuessCorrectly = true;
+        }
+        Log.d(TAG, "new number is " + userGuessCorrectly);
+        return userGuessCorrectly;
+    }
+
+    void changeGuessButtonState(boolean enable) {
+
+/*        float alphaValue = .75f;
+        if (enable) {
+            alphaValue = 1.0f;
+        }
+        mEvenButton.setClickable(enable);
+        mEvenButton.setAlpha(alphaValue);
+        mOddButton.setClickable(enable);
+        mOddButton.setAlpha(alphaValue);  */
+
+        mOddButton.setBackgroundColor(Color.BLACK);
+        mOddButton.setTextColor(Color.WHITE);
+        mEvenButton.setBackgroundColor(Color.BLACK);
+        mEvenButton.setTextColor(Color.WHITE);
+
+    }
+    void changeAgainButtonState() {
+        mAgainButton.setBackgroundColor(Color.parseColor("#00bfff"));
+        mAgainButton.setTextColor(Color.WHITE);
     }
 }
